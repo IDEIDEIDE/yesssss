fd4227b76350b57aee6c35c45a1f4e73bf35f2f0
IDEIDEIDE <75457931+IDEIDEIDE@users.noreply.github.com>
2021-06-29 13:55:14 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,27 @@ function preload(){
 function setup(){
     canvas = createCanvas(300, 300)
     canvas.center()
+    video = createCapture(VIDEO)
+    video.size(300, 300)
+    video.hide()
+
+    PoseNet = ml5.poseNet(video, modelLoaded)
+    PoseNet.on('pose', gotResults)
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+}
+
+function gotResults(results){
+    if(results.length > 0){
+        console.log("the x is " + results[0].pose.nose.x)
+        console.log("the y is " + results[0].pose.nose.y)
+    }
 }
 
 function draw(){
+image(video, 0, 0, 300, 300)
 
 }
 
